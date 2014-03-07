d016c8cd10ab016f8f922b9f66cd6f66e6ad0cca
bing <tenexbing@gmail.com>
2014-03-07 11:57:56 +0800

    init

---
+++ Meow/Meow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Meow
+//
+//  Created by b on 3/7/14.
+//  Copyright (c) 2014 b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Meow/Meow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Meow
+//
+//  Created by b on 3/7/14.
+//  Copyright (c) 2014 b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
